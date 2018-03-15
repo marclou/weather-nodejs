207f4a5ed312e952f655cc0353736d44232fe3fc
marclou <marc.louvion@gmail.com>
2018-03-15 16:18:09 +0900

    first commit, contains mains functions

---
+++ app.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const GOOGLE_MAPS_KEY = 'AIzaSyDtwq31X33G-bMpcmdzOp49GrNV195pvBM';
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			description: 'Address to fetch the weather',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+const encodedAddress = encodeURIComponent(argv.a || argv.address);
+
+request({
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${GOOGLE_MAPS_KEY}`,
+	json: true
+}, (error, response, body) => {
+	if (!error) {
+		console.log('Error with Google address');
+		return;
+	}
+	const {
+		formatted_address,
+		geometry
+	} = body.results[0];
+
+	console.log(`Address : ${formatted_address} \nLattitude : ${geometry.location.lat}, \nLongitude : ${geometry.location.lng}`);
+});
